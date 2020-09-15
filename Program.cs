154c09e21d10b878242ed0472d859adb64713734
arvin <arvinmojj@users.noreply.github.com>
2020-09-15 10:46:44 +0430

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Typing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string EnterText;
+            string EnterCheckText;
+            Console.Write("Enter Text : ");
+            EnterText = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (true)
+            {
+                if (true)
+                {
+                    Console.Write("Text : ");
+                    EnterCheckText = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                
+                if (EnterText == EnterCheckText)
+                {
+                    System.Console.WriteLine(EnterText);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+
+                else
+                {
+                    Console.WriteLine(EnterText);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+            }
+        }
+    }
+}
